89b6febdfc43ef0bcc7aa89662b48aa6fa7bfaa2
geekCoder01 <63516933+ankur786-coder@users.noreply.github.com>
2022-04-06 13:48:11 +0530

    adding form

---
+++ 2 React State & Working with events/3 Adding Form Inputs/src/App.js
@@ -0,0 +1,40 @@
+//import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+//In this we can pass data using props like here
+//we make const expenses in App and gives values in json format
+// and add into <expense> with its title,date,amount and pass props as a argument in 
+//ExpenseItem and assign values as {props.title} and so on.....
+
+function App() {
+  const expenses=[
+      {
+        id: 'e1',
+        title: 'Toilet Paper',
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+      },
+      { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+      {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+      },
+      {
+        id: 'e4',
+        title: 'New Desk (Wooden)',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+      },
+  ];
+
+  return (
+    <div>
+      <NewExpense/>
+      <Expenses items={expenses}/>
+    </div>
+  );
+}
+
+export default App;
