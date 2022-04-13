de388dd61f27dd7e7bd921a66bea8fc5eaad6bc7
geekCoder01 <63516933+ankur786-coder@users.noreply.github.com>
2022-04-13 11:43:41 +0530

    adding more expenses

---
+++ 3 Rendering Lists & Conditional Content/2 using stateful list/App.js
@@ -0,0 +1,49 @@
+//import ExpenseItem from "./components/ExpenseItem";
+import React,{useState} from 'react';
+import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+//in this we are adding more expenses and also relfect it to actual console by using state 
+//we take const expenses as dummy expenses to initialize and use method to display it on the 
+//application
+
+const DUMMY_EXPENSES=[
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+function App() {
+  const[expenses,setExpenses]=useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler =(expense)=>{
+      setExpenses((prevExpenses)=>{
+        return [expense, ...prevExpenses];
+      });
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses}/>
+    </div>
+  );
+}
+
+export default App;
